e3065f3f3e16a4f557cfd91700de6aac30dba84a
chasyuss <cotn963@gmail.com>
2024-04-25 21:28:55 +0900

    [feat]: add search

--- js/search.js
+++ js/search.js
@@ -1,14 +1,27 @@
-<script src="./js/movie.js"></script>
-// 카드 요소들 가져오기
-const cards = document.querySelectorAll('.moive-card');
+function filter() {
+    // 검색어를 가져와 대문자로 변환합니다.
+    let value = document.getElementById("value").value.toUpperCase();
 
-// 각 카드에 클릭 이벤트 리스너 추가
-cards.forEach(card => {
-  card.addEventListener('click', function() {
-    // 해당 카드의 데이터 ID 가져오기
-    const cardId = card.dataset.id;
-    
-    // alert 창으로 ID 표시
-    alert('Clicked card ID: ' + cardId);
-  });
-});
+    // 모든 영화 타이틀 요소를 가져옵니다.
+    let listInner = document.getElementsByClassName("movie-card");
+
+    // 각 영화 타이틀 요소를 검사합니다.
+    for (let i = 0; i < listInner.length; i++) {
+        // 현재 요소의 타이틀을 가져옵니다.
+        let title = listInner[i].getElementsByClassName("movie_title")[0].innerHTML.toUpperCase();
+
+        // 검색어가 타이틀에 포함되어 있는지 확인합니다.
+        if (title.indexOf(value) !== -1) {
+            listInner[i].style.display = "block";  // 검색어가 포함되어 있으면 표시 
+            //flex로 했더니 전부다 옆으로 밀리는 현상이 발생함
+        } else {
+            listInner[i].style.display = "none";  // 포함되어 있지 않으면 숨깁니다.
+        }
+    }
+}
+
+function preventEnter(event) {
+    if (event.key === "Enter") { // 엔터키인지 확인
+        event.preventDefault(); // 엔터키로 폼 제출 방지
+    }
+}
