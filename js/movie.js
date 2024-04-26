ef1f9278f75bd68ad7a095ecbb8169c23caf2a54
chasyuss <cotn963@gmail.com>
2024-04-26 11:17:59 +0900

    [refactor] api 복구

--- js/movie.js
+++ js/movie.js
@@ -2,7 +2,7 @@ const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-    //   Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhOGU4ZTc4OTU5YTZlNmQ1NjFhMTJkOTBlNDBiOTFlNSIsInN1YiI6IjY2MjhjN2ZkNGE0YmY2MDE4ODc3NjBjOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Rd6KtbDgPmuaiQBVsIMOWT4Ed5GqmKrQXeOx9qfW4-8'
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhOGU4ZTc4OTU5YTZlNmQ1NjFhMTJkOTBlNDBiOTFlNSIsInN1YiI6IjY2MjhjN2ZkNGE0YmY2MDE4ODc3NjBjOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Rd6KtbDgPmuaiQBVsIMOWT4Ed5GqmKrQXeOx9qfW4-8'
     }
   };
 
